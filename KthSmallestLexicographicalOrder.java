8c817ea7d355af7a0a86f992a365ff32315d5733
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2025-06-09 23:07:29 +0530

    PNo-440

--- KthSmallestLexicographicalOrder.java
+++ KthSmallestLexicographicalOrder.java
@@ -1,3 +1,17 @@
+
+/*==================================================================================
+  
+Given two integers n and k, return the kth lexicographically smallest integer in 
+the range [1, n]. 
+
+Example 1:
+
+Input: n = 13, k = 2
+Output: 10
+Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9],
+so the second smallest number is 10.
+======================================================================================*/
+
 class KthSmallestLexicographicalOrder {
     public static int countSkipNum(long curr, long next, int n) {
         int cntNum = 0;
